88545f2e42bb67a63c9a2da37a983d50a0998381
Javier Fuchs <javier.fuchs@gmail.com>
2015-07-21 00:53:20 -0300

    Adding workspace and COTDImage

---
+++ COTD/COTD/COTDImage.h
@@ -0,0 +1,13 @@
+//
+//  COTDImage.h
+//  COTD
+//
+//  Created by Javier Fuchs on 7/21/15.
+//  Copyright (c) 2015 Javier Fuchs. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface COTDImage : PFObject
+
+@end
---
+++ COTD/COTD/COTDImage.m
@@ -0,0 +1,13 @@
+//
+//  COTDImage.m
+//  COTD
+//
+//  Created by Javier Fuchs on 7/21/15.
+//  Copyright (c) 2015 Javier Fuchs. All rights reserved.
+//
+
+#import "COTDImage.h"
+
+@implementation COTDImage
+
+@end
